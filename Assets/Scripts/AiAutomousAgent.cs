112b1f0da2c8b7e6428a075736b44ac9a7c29b75
Michael Patino <mpatino@student.neumont.edu>
2024-01-17 18:53:03 -0700

    Added SphereCast

--- Assets/Scripts/AiAutomousAgent.cs
+++ Assets/Scripts/AiAutomousAgent.cs
@@ -1,159 +1,143 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class AIAutonomousAgent : AIAgent
 {
-    [SerializeField] AIPerception seekPerception = null;
-    [SerializeField] AIPerception fleePerception = null;
-    [SerializeField] AIPerception flockPerception = null;
-    [SerializeField] AIPerception obsticalPerception = null;
+	public AIPerception seekPerception = null;
+	public AIPerception fleePerception = null;
+	public AIPerception flockPerception = null;
+	public AIPerception obstaclePerception = null;
 
-    private Vector3 WanderTarget;
+	private void Update()
+	{
+		// seek
+		if (seekPerception != null)
+		{
+			var gameObjects = seekPerception.GetGameObjects();
+			if (gameObjects.Length > 0)
+			{
+				Vector3 force = Seek(gameObjects[0]);
+				movement.ApplyForce(force);
+			}
+		}
 
-    private void Start()
-    {
-        WanderTarget = Vector3.zero;
-    }
-    private void Update()
-    {
-        // seek
-        if (seekPerception != null)
-        {
-            var gameObjects = seekPerception.GetGameObjects();
-            if (gameObjects.Length > 0)
-            {
-                Vector3 force = Seek(gameObjects[0]);
-                movement.ApplyForce(force);
+		// flee
+		if (fleePerception != null)
+		{
+			var gameObjects = fleePerception.GetGameObjects();
+			if (gameObjects.Length > 0)
+			{
+				Vector3 force = Flee(gameObjects[0]);
+				movement.ApplyForce(force);
+			}
+		}
 
-                Debug.Log("Seeking Target: " + seekPerception.GetGameObjects()[0].name);
+		// flock
+		if (flockPerception != null)
+		{
+			var gameObjects = flockPerception.GetGameObjects();
+			if (gameObjects.Length > 0)
+			{
+				movement.ApplyForce(Cohesion(gameObjects));
+				movement.ApplyForce(Separation(gameObjects, 3));
+				movement.ApplyForce(Alignment(gameObjects));
+			}
 
-            }
-        }
+			// wrap position in world
+			transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
+		}
 
-        // flee
-        if (fleePerception != null)
-        {
-            var gameObjects = fleePerception.GetGameObjects();
-            if (gameObjects.Length > 0)
-            {
-                Vector3 force = Flee(gameObjects[0]);
-                movement.ApplyForce(force);
+		// obstacle avoidance
+		if (obstaclePerception != null)
+		{
+			if (((AISphereCastPerception)obstaclePerception).CheckDirection(Vector3.forward))
+			{
+				Vector3 open = Vector3.zero;
+				if (((AISphereCastPerception)obstaclePerception).GetOpenDirection(ref open))
+				{
+					movement.ApplyForce(GetSteeringForce(open));
+				}
+			}
+			var gameObjects = obstaclePerception.GetGameObjects();
+		}
 
-                Debug.Log("fleeing Target: " + fleePerception.GetGameObjects()[0].name);
+		// cancel y movement
+		Vector3 acceleration = movement.Acceleration;
+		acceleration.y = 0;
+		movement.Acceleration = acceleration;
+	}
 
-            }
-        }
+	private Vector3 Seek(GameObject target)
+	{
+		Vector3 direction = target.transform.position - transform.position;
+		Vector3 force = GetSteeringForce(direction);
 
-        // flock
-        if (flockPerception != null)
-        {
-            var gameObjects = flockPerception.GetGameObjects();
-            if (gameObjects.Length > 0)
-            {
-                movement.ApplyForce(Cohesion(gameObjects));
-                movement.ApplyForce(Separation(gameObjects, 3));
-                movement.ApplyForce(Alignment(gameObjects));
+		return force;
+	}
 
-                Debug.Log("flocking: " + flockPerception.GetGameObjects()[0].name);
+	private Vector3 Flee(GameObject target)
+	{
+		Vector3 direction = transform.position - target.transform.position;
+		Vector3 force = GetSteeringForce(direction);
 
-            }
-        }
-        //obstacle perception
-        if (obsticalPerception != null)
-        {
-            if (((AIRaycastPerception)obsticalPerception).CheckDirection(transform.forward))
-            {
-                Vector3 open = Vector3.zero;
-                if(((AIRaycastPerception)obsticalPerception).GetOpenDirection(ref open))
-                {
-                    movement.ApplyForce(GetSteeringForce(open) * 5);
-                }
+		return force;
+	}
 
-            }
-        }
+	private Vector3 Cohesion(GameObject[] neighbors)
+	{
+		Vector3 positions = Vector3.zero;
+		foreach (var neighbor in neighbors)
+		{
+			positions += neighbor.transform.position;
+		}
 
-        //cancel y movement
-        Vector3 acceleration = movement.Acceleration;
-        acceleration.y = 0;
-        movement.Acceleration = acceleration;
+		Vector3 center = positions / neighbors.Length;
+		Vector3 direction = center - transform.position;
 
+		Vector3 force = GetSteeringForce(direction);
 
-        transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
-    }
+		return force;
+	}
 
-    private Vector3 Seek(GameObject target)
-    {
-        Vector3 direction = target.transform.position - transform.position;
-        Vector3 force = GetSteeringForce(direction);
+	private Vector3 Separation(GameObject[] neighbors, float radius)
+	{
+		Vector3 separation = Vector3.zero;
+		foreach (var neighbor in neighbors)
+		{
+			Vector3 direction = (transform.position - neighbor.transform.position);
+			if (direction.magnitude < radius)
+			{
+				separation += direction / direction.sqrMagnitude;
+			}
+		}
 
-        return force;
-    }
+		Vector3 force = GetSteeringForce(separation);
 
-    private Vector3 Flee(GameObject target)
-    {
-        Vector3 direction = transform.position - target.transform.position;
-        Vector3 force = GetSteeringForce(direction);
+		return force;
+	}
 
-        return force;
-    }
+	private Vector3 Alignment(GameObject[] neighbors)
+	{
+		Vector3 velocities = Vector3.zero;
+		foreach (var neighbor in neighbors)
+		{
+			velocities += neighbor.GetComponent<AIAgent>().movement.Velocity;
+		}
+		Vector3 averageVelocity = velocities / neighbors.Length;
 
-    private Vector3 Cohesion(GameObject[] neighbors)
-    {
-        Vector3 positions = Vector3.zero;
-        foreach (var neighbor in neighbors)
-        {
-            positions += neighbor.transform.position;
-        }
+		Vector3 force = GetSteeringForce(averageVelocity);
 
-        Vector3 center = positions / neighbors.Length;
-        Vector3 direction = center - transform.position;
+		return force;
+	}
 
-        Vector3 force = GetSteeringForce(direction);
+	private Vector3 GetSteeringForce(Vector3 direction)
+	{
+		Vector3 desired = direction.normalized * movement.maxSpeed;
+		Vector3 steer = desired - movement.Velocity;
+		Vector3 force = Vector3.ClampMagnitude(steer, movement.maxForce);
 
-        return force;
-    }
-
-    private Vector3 Separation(GameObject[] neighbors, float radius)
-    {
-        Vector3 separation = Vector3.zero;
-        foreach (var neighbor in neighbors)
-        {
-            // get direction vector away from neighbor
-            Vector3 direction = (transform.position - neighbor.transform.position);
-            // check if within separation radius
-            if (direction.magnitude < radius)
-            {
-                // scale separation vector inversely proportional to the direction distance
-                // closer the distance the stronger the separation
-                separation += direction / direction.sqrMagnitude;
-            }
-        }
-
-        Vector3 force = GetSteeringForce(separation);
-
-        return force;
-    }
-
-    private Vector3 Alignment(GameObject[] neighbors)
-    {
-        Vector3 velocities = Vector3.zero;
-        foreach (var neighbor in neighbors)
-        {
-            velocities += neighbor.GetComponent<AIAgent>().movement.Velocity;
-        }
-        Vector3 averageVelocity = velocities / neighbors.Length;
-
-        Vector3 force = GetSteeringForce(averageVelocity);
-        return force;
-    }
-
-    private Vector3 GetSteeringForce(Vector3 direction)
-    {
-        Vector3 desired = direction.normalized * movement.maxSpeed;
-        Vector3 steer = desired - movement.Velocity;
-        Vector3 force = Vector3.ClampMagnitude(steer, movement.maxForce);
-
-        return force;
-    }
+		return force;
+	}
 }
