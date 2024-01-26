0c9d75fa9eda9c002c49b6f5992d8f6327ac4116
Michael Patino <mpatino@student.neumont.edu>
2024-01-26 01:38:21 -0700

    implemented basic nav mesh

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playerController : MonoBehaviour
+{
+	public Camera cam;
+	public NavMeshAgent agent;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				//Move Agent
+				agent.SetDestination(hit.point);
+			} //shoots out a ray true if hits something false if not
+		}
+
+	}
+}
