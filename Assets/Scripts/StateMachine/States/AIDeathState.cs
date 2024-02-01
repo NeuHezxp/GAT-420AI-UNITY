b46c54230292ad8638317bb151e450f13132e1cf
Michael Patino <mpatino@student.neumont.edu>
2024-01-31 22:12:07 -0700

    states

---
+++ Assets/Scripts/StateMachine/States/AIDeathState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIDeathState : AIState
+{
+    public AIDeathState(AIStateAgent agent) : base(agent)
+    {
+    }
+
+    public override void OnEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/StateMachine/States/AIPatrolState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPatrolState : AIState
+{
+    public AIPatrolState(AIStateAgent agent) : base(agent)
+    {
+    }
+
+    public override void OnEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
